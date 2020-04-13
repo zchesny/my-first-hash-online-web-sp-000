96675bae1b182e6bf2038f04e8bb74c1739fdc35
Zoey Chesny <z.chesny@gmail.com>
2020-04-13 15:58:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -42,7 +42,7 @@ def adding
 
 
   # add 4 gun powder to the shipping_manifest hash below
-  shipping_manifest["gun powerder" = 4]
+  shipping_manifest["gun powerder"] = 4
 
 
   # return the shipping_manifest hash below
